898242513e22e2902200eaa6752b697e80fc7e3a
Florian <florian.rieder@unil.ch>
2021-02-20 21:36:56 +0100

    stuuuuffffffff

--- NPCDialog.gd
+++ NPCDialog.gd
@@ -8,12 +8,9 @@ func _ready():
 	GameEvents.connect("dialog_finished", self, "_on_dialog_finished")
 
 func _on_Area2D_body_entered(body):
-	print(_has_been_played)
 	if body.name == "Player" and not _has_been_played:
 		GameEvents.emit_dialog_initiated(_dialogue)
 
 func _on_dialog_finished(dialog) -> void:
-	print("here")
 	if dialog == _dialogue:
-		print("there")
 		_has_been_played = true
