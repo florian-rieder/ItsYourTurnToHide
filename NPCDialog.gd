6bf99d7a860184a38550747e98fbd02f8661a658
Florian <florian.rieder@unil.ch>
2021-02-20 18:15:38 +0100

    dialogs can only be triggered once

--- NPCDialog.gd
+++ NPCDialog.gd
@@ -2,6 +2,18 @@ extends Node2D
 
 export(Resource) var _dialogue = _dialogue as Dialogue
 
+var _has_been_played = false
+
+func _ready():
+	GameEvents.connect("dialog_finished", self, "_on_dialog_finished")
+
 func _on_Area2D_body_entered(body):
-	if body.name == "Player":
+	print(_has_been_played)
+	if body.name == "Player" and not _has_been_played:
 		GameEvents.emit_dialog_initiated(_dialogue)
+
+func _on_dialog_finished(dialog) -> void:
+	print("here")
+	if dialog == _dialogue:
+		print("there")
+		_has_been_played = true
--- game_events.gd
+++ game_events.gd
@@ -1,12 +1,12 @@
 extends Node
 
 signal dialog_initiated(dialogue)
-signal dialog_finished
+signal dialog_finished(dialogue)
 
 func emit_dialog_initiated(dialogue : Dialogue) -> void:
 	# prevents last click to close dialog from being considered as a real click,
 	# avoiding clicking on food when closing dialog
 	call_deferred("emit_signal", "dialog_initiated", dialogue)
 
-func emit_dialog_finished() -> void:
-	call_deferred("emit_signal", "dialog_finished")
+func emit_dialog_finished(dialogue : Dialogue) -> void:
+	call_deferred("emit_signal", "dialog_finished", dialogue)
