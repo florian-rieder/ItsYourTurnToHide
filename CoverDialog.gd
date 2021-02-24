18385a75efc33ed9b4b244c9ee809d079e93491b
Florian <florian.rieder@unil.ch>
2021-02-24 15:05:07 +0100

    better coverdialog

--- CoverDialog.gd
+++ CoverDialog.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 
+export(bool) var move_on_found = true
+export(Vector2) var move_offset = Vector2(10, 0)
+
 onready var dialog = $NPCDialog
 onready var cover = $Cover
+
 
 var _has_been_triggered = false
 
@@ -15,3 +19,5 @@ func triggerDialog():
 		dialog.set_collider(false)
 		GameEvents.emit_signal("child_found")
 		_has_been_triggered = true
+		if move_on_found:
+			$NPCDialog/NPC.global_position += move_offset
