3077f0507dd65103cc4ae405a71753909c877ec0
Sergenti <florian.rieder@unil.ch>
2022-10-07 16:50:53 +0200

    fixed radio offset after deactivation/reactivation
    yay !

--- Radio.gd
+++ Radio.gd
@@ -9,28 +9,32 @@ onready var sound = $AudioStreamPlayer2D
 var _on = true
 
 func _process(delta):
-	if _on:
-		get_tree().call_group("Ennemies","distracted",global_position)
+    if _on:
+        get_tree().call_group("Ennemies","distracted",global_position)
 
 func activate():
-	if _on:
-		sound.stop()
-		$Radio.frame = 0
-		$Radio.playing = false
-		_on = false
-	else:
-		sound.play()
-		$Radio.playing = true
-		_on = true
+    if _on:
+        # Don't use sound.stop() !
+        # it makes it so that if you disable and reenable a radio, the music
+        # will be offset from the other radios !
+        sound.volume_db = -80
+        $Radio.frame = 0
+        $Radio.playing = false
+        _on = false
+    else:
+        #sound.play()
+        sound.volume_db = 0
+        $Radio.playing = true
+        _on = true
 
 
 func _on_Radio_body_entered(body):
-	if body.name == "Player":
-		body.canInteract(message,"Radio",self)
-	if body is RigidBody2D:
-		activate()
+    if body.name == "Player":
+        body.canInteract(message,"Radio",self)
+    if body is RigidBody2D:
+        activate()
 
 
 func _on_Radio_body_exited(body):
-	if body.name == "Player":
-		body.resetInteract()
+    if body.name == "Player":
+        body.resetInteract()
